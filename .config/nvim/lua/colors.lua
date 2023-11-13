aa0ab9138e41c0bba15843f29beebe1065fa4c7d
Max Gorin <regzok@gmail.com>
2023-11-13 12:03:48 +0200

    Further color tweaks

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,7 +1,13 @@
-vim.cmd 'colorscheme nightfox'
+vim.cmd 'colorscheme catppuccin'
 
 vim.cmd [[
   hi Folded guibg=NONE guifg=gray
+
+  " slightly darker background on active windows
+  hi Normal guifg=#cdd6f4 guibg=#1c1c1c
+
+  " slightly lighter background on inactive windows
+  hi NormalNC guifg=#cdd6f4 guibg=#202020
 ]]
 
 return false
