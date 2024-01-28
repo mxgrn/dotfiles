03e8ad3355e6c8e1b96c064b8bdd940caddc708f
Max Gorin <regzok@gmail.com>
2024-01-28 08:01:53 +0700

    Get back <c-h> and <c-l> from oil

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -76,12 +76,12 @@ require("lazy").setup({
         keymaps = {
           ["g?"] = "actions.show_help",
           ["<CR>"] = "actions.select",
+          ["<C-h>"] = false,
+          ["<C-l>"] = false,
           -- ["<C-s>"] = "actions.select_vsplit",
-          -- ["<C-h>"] = "actions.select_split",
           -- ["<C-t>"] = "actions.select_tab",
           -- ["<C-p>"] = "actions.preview",
           -- ["<esc>"] = "actions.close",
-          -- ["<C-l>"] = "actions.refresh",
           ["-"] = "actions.parent",
           ["_"] = "actions.open_cwd",
           ["`"] = "actions.cd",
@@ -101,7 +101,7 @@ require("lazy").setup({
             return vim.startswith(name, ".")
           end,
           -- This function defines what will never be shown, even when `show_hidden` is set
-          is_always_hidden = function(name, _)
+          is_always_hidden = function(_name, _)
             return false
           end,
           sort = {
