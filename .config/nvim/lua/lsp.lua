bd40e88d56335ef676655f7af7752975abf25849
Max Gorin <regzok@gmail.com>
2025-10-19 11:06:21 +0545

    Drop old LSP config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,99 +1,19 @@
-local lspconfig = require('lspconfig')
-local util = require 'lspconfig.util'
-
+-- Shared LSP settings
 local on_attach = function(_, bufnr)
-  local function map(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  local map = function(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true })
   end
-  local map_opts = { noremap = true, silent = true }
-  map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", map_opts)
-  map('n', '<space>d', '<cmd>lua vim.diagnostic.open_float()<cr>', map_opts)
-  -- Move to the next and previous diagnostic
-  map('n', '<space>[', '<cmd>lua vim.diagnostic.goto_next()<cr>', map_opts)
-  map('n', '<space>]', '<cmd>lua vim.diagnostic.goto_prev()<cr>', map_opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, map_opts)
-  vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format { async = true }' ]])
+
+  map("n", "K", vim.lsp.buf.hover)
+  map("n", "gr", vim.lsp.buf.references)
+  map("n", "<space>d", vim.diagnostic.open_float)
+  map("n", "<space>[", vim.diagnostic.goto_next)
+  map("n", "<space>]", vim.diagnostic.goto_prev)
+
+  vim.cmd([[command! Format execute 'lua vim.lsp.buf.format { async = true }']])
 end
 
-lspconfig.ts_ls.setup { on_attach = on_attach }
-
-lspconfig.lua_ls.setup {
-  on_attach = on_attach,
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- https://www.reddit.com/r/neovim/comments/khk335/lua_configuration_global_vim_is_undefined/
-        globals = { 'vim' }
-      }
-    }
-  }
-}
-
-lspconfig.sqlls.setup {
-  capabilities = vim.lsp.protocol.make_client_capabilities(),
-  on_attach = on_attach,
-  root_dir = util.find_git_ancestor,
-}
-
-lspconfig.dockerls.setup {
+-- Apply this to *every* LSP
+vim.lsp.config["*"] = {
   on_attach = on_attach,
 }
-
-lspconfig.jsonls.setup {}
-
--- html-lsp
-lspconfig.html.setup {
-  capabilities = vim.lsp.protocol.make_client_capabilities(),
-  on_attach = on_attach,
-}
-
-lspconfig.elmls.setup({
-  on_attach = on_attach,
-  settings = {
-    elmLS = {
-      onlyUpdateDiagnosticsOnSave = true
-    }
-  }
-})
-
-lspconfig.elixirls.setup {
-  cmd = { vim.fn.expand(vim.fn.stdpath('data') .. "/mason/packages/elixir-ls/language_server.sh") },
-  capabilities = vim.lsp.protocol.make_client_capabilities(),
-  on_attach = on_attach,
-  elixirLS = {
-    -- I choose to disable dialyzer for personal reasons, but
-    -- I would suggest you also disable it unless you are well
-    -- aquainted with dialzyer and know how to use it.
-    dialyzerEnabled = false,
-    -- I also choose to turn off the auto dep fetching feature.
-    -- It often get's into a weird state that requires deleting
-    -- the .elixir_ls directory and restarting your editor.
-    fetchDeps = false,
-  }
-}
-
--- local configs = require("lspconfig.configs")
---
--- local lexical_config = {
---   filetypes = { "elixir", "eelixir", "heex" },
---   cmd = { "/Users/mxgrn/code/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
---   settings = {},
--- }
---
--- if not configs.lexical then
---   configs.lexical = {
---     default_config = {
---       filetypes = lexical_config.filetypes,
---       cmd = lexical_config.cmd,
---       root_dir = function(fname)
---         return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
---       end,
---       -- optional settings
---       settings = lexical_config.settings,
---     },
---   }
--- end
---
--- lspconfig.lexical.setup({
---   on_attach = on_attach,
--- })
